642124ea9ce922774da2d3811229c18f77c5e357
Alex Landolt <alex.landolt@gmail.com>
2025-06-23 18:34:21 +0200

    Addition of controller + analyzer to mic class + cellposev4
    - analyzer and controller are also kinda mic dependant, hence added to the microscope class to further simplify and abstract.
    - Also add cellpose v4 with custom model selection

--- rtm_pymmcore/microscope/MMDemo.py
+++ rtm_pymmcore/microscope/MMDemo.py
@@ -1,6 +1,6 @@
-import os
 import pymmcore_plus
-from .abstract_microscope import AbstractMicroscope
+from rtm_pymmcore.microscope.abstract_microscope import AbstractMicroscope
+from rtm_pymmcore.controller import ControllerSimulated, Analyzer
 
 
 class MMDemo(AbstractMicroscope):
@@ -10,10 +10,11 @@ class MMDemo(AbstractMicroscope):
     USE_AUTOFOCUS_EVENT = False
     USE_ONLY_PFS = False
 
-    def __init__(self):
+    def __init__(self, old_data_project_path: str):
         super().__init__()
         pymmcore_plus.use_micromanager(self.MICROMANAGER_PATH)
         self.mmc = pymmcore_plus.CMMCorePlus()
+        self.old_data_project_path = old_data_project_path
         self.init_scope()
 
     def init_scope(self):
@@ -22,8 +23,16 @@ class MMDemo(AbstractMicroscope):
         self.mmc.setConfig(groupName="System", configName="Startup")
         self.mmc.setChannelGroup(channelGroup=self.CHANNEL_GROUP)
 
-    def run_experiment(self):
+    def run_experiment(self, df_acquire):
         """Run the experiment."""
+        self.analyzer = Analyzer(self.pipeline)
+        self.controller = ControllerSimulated(
+            self.analyzer,
+            self.mmc,
+            self.queue,
+            self.USE_AUTOFOCUS_EVENT,
+            project_path=self.old_data_project_path,
+        )
         pymmcore_plus.configure_logging(stderr_level="WARNING")
 
     def post_experiment(self):
